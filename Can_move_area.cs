3e051affe438adf4859af6433aa0904034788727
Tens0L <45615964+Tens0L@users.noreply.github.com>
2023-10-22 05:04:31 +0900

    Can_move_area.cs を更新

--- Can_move_area.cs
+++ Can_move_area.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//移動可能エリアでの動きをここにまとめたい
+//
+//
 public class Can_move_area : MonoBehaviour
 {
-
+    //移動先のマーカーオブジェクト
     [SerializeField]  GameObject spot_pref;
+    //移動先となるマーカーのポジションを保持する
     public Vector3 spot_pos;
 
+    //クリックされたときの動きだけ定義しておく
     public void ivent_reactor(Vector3 spot_v3)
     {
-
-
 
         //移動先spotの場所をクリックイベントから受け取ったvector3を代入
         spot_pref.transform.position = spot_v3;
